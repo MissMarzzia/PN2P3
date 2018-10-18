9f3223332657eba3fcd7e5782e3ab972ebf3752c
MissMarzzia <44242129+MissMarzzia@users.noreply.github.com>
2018-10-18 09:27:18 -0300

    Create FACTURADETALLE.java

---
+++ FACTURADETALLE.java
@@ -0,0 +1,20 @@
+public class FACTURADETALLE {
+    private int cantidad;
+
+    public FACTURADETALLE() {
+    }
+
+    public FACTURADETALLE(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+}
