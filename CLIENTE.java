b3336418b995b5546f14cbee5c376260b9028b46
MissMarzzia <44242129+MissMarzzia@users.noreply.github.com>
2018-10-18 09:29:09 -0300

    Create CLIENTE.java

---
+++ CLIENTE.java
@@ -0,0 +1,13 @@
+public class CLIENTE {
+    private String denominacion,
+            cuit;
+
+    public CLIENTE() {
+    }
+
+    public CLIENTE(String denominacion, String cuit) {
+        this.denominacion = denominacion;
+        this.cuit = cuit;
+    }
+   
+}
