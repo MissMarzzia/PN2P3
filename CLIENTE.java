3e952b7cdc86c66ca91268b12df173699d535114
Florencia Zapata <38169341+flodolce@users.noreply.github.com>
2018-10-18 09:34:49 -0300

    Update CLIENTE.java
    Agregué la relación con Domicilio.

--- CLIENTE.java
+++ CLIENTE.java
@@ -1,13 +1,38 @@
 public class CLIENTE {
     private String denominacion,
             cuit;
+    private DOMICILIO domicilioCliente;
 
-    public CLIENTE() {
-    }
-
-    public CLIENTE(String denominacion, String cuit) {
+   public CLIENTE(String denominacion, String cuit, DOMICILIO domicilioCliente) {
         this.denominacion = denominacion;
         this.cuit = cuit;
+        this.domicilioCliente = domicilioCliente;
     }
-   
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public String getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(String cuit) {
+        this.cuit = cuit;
+    }
+
+    public DOMICILIO getDomicilioCliente() {
+        return domicilioCliente;
+    }
+
+    public void setDomicilioCliente(DOMICILIO domicilioCliente) {
+        this.domicilioCliente = domicilioCliente;
+    }
+    
+    
+    
 }
